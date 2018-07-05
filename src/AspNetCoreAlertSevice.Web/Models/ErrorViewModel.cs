3583a85c1e1a23966a1816ce14b6756f029244a4
johnmarsing <johnmarsing@gmail.com>
2018-07-04 21:21:37 -0700

    first-commit

---
+++ src/AspNetCoreAlertSevice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreAlertSevice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/AspNetCoreAlertSevice.Web/ViewComponents/AlertsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreAlertSevice.Web.Features.Alerts;
+
+namespace AspNetCoreAlertSevice.Web.ViewComponents
+{
+	public class AlertsViewComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var alerts = TempData.ContainsKey(Alert.TempDataKey)
+			? (List<Alert>)TempData[Alert.TempDataKey]
+			: new List<Alert>();
+
+			return View(alerts);
+		}
+	}
+}
